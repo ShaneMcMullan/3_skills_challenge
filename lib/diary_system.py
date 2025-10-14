f2c77524c668b5cff928687678eb94444d3665fd
ShaneMcMullan <shanemcmullan@icloud.com>
2025-10-14 16:26:07 +0100

    Updates

--- lib/diary_system.py
+++ lib/diary_system.py
@@ -1,3 +1,11 @@
-def make_snippet(str):
-    new_str = str.strip().split()
-    return " ".join(new_str[:5])+"..."
+def make_snippet(text):
+
+    if type(text) is not str:
+        raise Exception("A string has not been entered.")
+    
+    new_str = text.strip().split()
+    
+    if len(new_str) > 5:
+        return " ".join(new_str[:5])+"..."
+    else:
+        return " ".join(new_str)
--- tests/test_diary_system.py
+++ tests/test_diary_system.py
@@ -1,4 +1,5 @@
 from lib.diary_system import make_snippet
+import pytest
 
 def test_first_five_words():
     result = make_snippet("Today I went to the park and had a walk.")
@@ -6,5 +7,11 @@ def test_first_five_words():
     
 def test_str_less_than_five_words():
     result = make_snippet("Today I went")
-    assert result == "Today I went..."
+    assert result == "Today I went"
 
+def test_not_string_argument():
+    
+    with pytest.raises(Exception) as e:
+        result = make_snippet(55)
+    error_message = str(e.value)
+    assert error_message == "A string has not been entered."
